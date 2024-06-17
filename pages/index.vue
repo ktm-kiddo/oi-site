57001995206c17bf26d2038fba79c9217c60dd2b
Peter Graif <peter@theopen.institute>
2024-06-17 18:08:36 +0545

    repaired

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 const route = useRoute();
 </script>
 
@@ -7,7 +7,6 @@ const route = useRoute();
         <h1 class="text-blue-500">Nuxt Routing set up successfully!</h1>
         <p>Current route: {{ route.path }}</p>
         <p>{{ 1 + 1 }}</p>
-        <p>{{ route }}</p>
         <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
     </div>
 </template>

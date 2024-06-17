893653c2c857faeac67951f2caabe5b2f85b6677
Peter Graif <peter@theopen.institute>
2024-06-17 17:52:59 +0545

    Peter's temp push

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
-<script setup lang="ts">
+<script setup>
 const route = useRoute();
 </script>
 
 <template>
-  <div>
-    <h1 class="text-blue-500">Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
-      >Learn more about Nuxt Routing</a
-    >
-  </div>
+    <div>
+        <h1 class="text-blue-500">Nuxt Routing set up successfully!</h1>
+        <p>Current route: {{ route.path }}</p>
+        <p>{{ 1 + 1 }}</p>
+        <p>{{ route }}</p>
+        <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    </div>
 </template>

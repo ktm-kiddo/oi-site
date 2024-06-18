f6baebcba72b784bc83150449476bd5716359eca
Mateo Graif <mateograif@gmail.com>
2024-06-18 07:46:34 +0545

    component directory added, navbar installed. customization needed

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,13 @@ const route = useRoute();
 </script>
 
 <template>
-    <div>
-        <h1 class="text-blue-500">Nuxt Routing set up successfully!</h1>
-        <p>Current route: {{ route.path }}</p>
-        <p>{{ 1 + 1 }}</p>
-        <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-    </div>
+  <div>
+    <Nav></Nav>
+    <h1 class="text-blue-500">Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <p>{{ 1 + 1 }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
+      >Learn more about Nuxt Routing</a
+    >
+  </div>
 </template>

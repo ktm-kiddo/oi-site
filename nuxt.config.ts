42374d789cfb6774027b467f5fc9243a303dac38
Mateo Graif <mateograif@gmail.com>
2024-06-22 09:26:18 +0545

    changed baseURL to support images in github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/oi-site/'
+    },
     devtools: { enabled: true },
     modules: ["@nuxtjs/tailwindcss", "nuxt-headlessui"],
 });

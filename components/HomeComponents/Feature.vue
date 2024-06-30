1b4d746469303869c9368a7fe10e9f017ea5ec4a
Mateo Graif <mateograif@gmail.com>
2024-06-30 18:36:12 -0400

    Added content to feature

--- components/HomeComponents/Feature.vue
+++ components/HomeComponents/Feature.vue
@@ -7,17 +7,15 @@
         <div class="px-6 lg:px-0 lg:pr-4 lg:pt-4">
           <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
             <h2 class="text-base font-semibold leading-7 text-oi-blue">
-              Deploy faster
+              The Open Institute for Social Science
             </h2>
             <p
               class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
             >
-              A better workflow
+              Our Mission
             </p>
             <p class="mt-6 text-lg leading-8 text-gray-600">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-              impedit perferendis suscipit eaque, iste dolor cupiditate
-              blanditiis ratione.
+              A new way to teach and practice social science
             </p>
             <dl
               class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none"
@@ -74,26 +72,29 @@ import {
   CloudArrowUpIcon,
   LockClosedIcon,
   ServerIcon,
+  EyeIcon,
+  UserIcon,
+  BookOpenIcon,
 } from "@heroicons/vue/20/solid";
 
 const features = [
   {
-    name: "Push to deploy.",
+    name: "Vision.",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: CloudArrowUpIcon,
+      "The Open Institute for Social Science is a member-driven academic community. We believe that high-quality social research will be the substance of a more innovative, more compassionate, and more equitable world. To get there, we are searching for new and better ways to practice our craft.",
+    icon: EyeIcon,
   },
   {
-    name: "SSL certificates.",
+    name: "People.",
     description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
-    icon: LockClosedIcon,
+      "The Open Institute for Social Science is an academic cooperative. We own no property, we generate no profits, and we have no employees. Very little of our work is centrally planned. Instead, our projects are driven by the passions of our membership, who coordinate and lead their social scientific efforts on our common platform.",
+    icon: UserIcon,
   },
   {
-    name: "Database backups.",
+    name: "Academics.",
     description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
-    icon: ServerIcon,
+      "The Open Institute for Social Science is an independent center of academic excellence, committed without compromise to local and international best practices. Though we aspire to be a resource to the worlds of policy and development, we are financially autonomous and thus free to be a very different kind of voice.",
+    icon: BookOpenIcon,
   },
 ];
 </script>

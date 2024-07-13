4876c56cc5ddcb27f0420f9fa264b23b90d622d0
Mateo Graif <mateograif@gmail.com>
2024-07-13 08:48:26 -0400

    undo

--- pages/index.vue
+++ pages/index.vue
@@ -17,28 +17,5 @@ const route = useRoute();
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
       >Learn more about Nuxt Routing</a
     > -->
-    <form name="contact" method="POST" netlify>
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label
-          >Your Role:
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label
-        >
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
   </div>
 </template>
